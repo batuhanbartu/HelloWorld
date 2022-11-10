c106b60061903bb72dcc9b23d157db241db29e67
batuhanbartu <batuhanmurat07@gmail.com>
2022-11-10 15:04:03 +0300

    Çarpım tablosu eklendi.

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,12 +1,19 @@
 ﻿#include <iostream>
 
+void carpimTablosu() {
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			std::cout << i * j << std::endl;
+		}
+	}
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Hello!";
 
-	for (int i = 0; i < 10; i++)
-	{
-		std::cout << i << std::endl;
-	}
+	carpimTablosu();
 }
